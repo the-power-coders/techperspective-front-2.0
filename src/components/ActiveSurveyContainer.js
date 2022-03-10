0a9eb37f52e8bf1287f3cdfbe58a22afdbe94382
Brian Thornburg <briant3275@gmail.com>
2022-03-10 17:06:59 -0600

    filter drop down

--- src/components/ActiveSurveyContainer.js
+++ src/components/ActiveSurveyContainer.js
@@ -6,60 +6,63 @@ import CreateSurveyForm from "./CreateSurveyForm";
 import { Button } from 'react-bootstrap';
 
 class ActiveSurveyContainer extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			showCreateSurvey: false
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            showCreateSurvey: false
+        }
+    }
 
-	//be able to close the create survey form
-	handleCloseCreateSurvey 
+    //be able to close the create survey form
+    handleCloseCreateSurvey
 
-  componentDidMount() {
-    this.props.getActiveSurvey();
-  }
-  render() {
-    return (
-      <>
-        <br></br>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          {this.props.activeSurvey === null && (
-            <NewSurveyButton createNewSurvey={this.props.createNewSurvey} />
-          )}
-          {this.props.activeSurvey && (
-						<>
-            <ActiveSurvey
-              graphResults={this.props.graphResults}
-              activeSurvey={this.props.activeSurvey}
-              openModal={this.props.openModal}
-              getActiveSurvey={this.props.getActiveSurvey}
-            />
-						{/* We found a better way! populating the update in the surveysummarylist instead of "On create" */}
-          	{/* <SurveyNoteForm 
+    componentDidMount() {
+        this.props.getActiveSurvey();
+    }
+    render() {
+        return (
+            <>
+                <br></br>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    {this.props.activeSurvey === null && (
+                        <NewSurveyButton createNewSurvey={this.props.createNewSurvey} />
+                    )}
+                    {this.props.activeSurvey && (
+                        <>
+                            <ActiveSurvey
+                                graphResults={this.props.graphResults}
+                                activeSurvey={this.props.activeSurvey}
+                                openModal={this.props.openModal}
+                                getActiveSurvey={this.props.getActiveSurvey}
+                            />
+                            {/* We found a better way! populating the update in the surveysummarylist instead of "On create" */}
+                            {/* <SurveyNoteForm 
 						handleUpdateSurvey={this.props.handleUpdateSurvey}
 						activeSurvey={this.props.activeSurvey}
 						/> */}
-						</>
-          )}
-        </div>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          {/* dropdown goes here  props surveyIdList handleSelect*/}
-          <SurveyDropdown
-            surveyIdList={this.props.surveyIdList}
-            handleSelectedSurvey={this.props.handleSelectedSurvey}
-          />
-					{
-					this.state.showCreateSurvey ? 
-          <CreateSurveyForm onClick={() => this.setState({showCreateSurvey: false})}insertSurveyToDb={this.props.insertSurveyToDb}/>
-					:
-					<Button onClick={() => this.setState({showCreateSurvey: true})}>Add Survey</Button>
-					}
-        </div>
-        <br></br>
-      </>
-    );
-  }
+                        </>
+                    )}
+                </div>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    {/* dropdown goes here  props surveyIdList handleSelect*/}
+                    <SurveyDropdown
+                        surveyIdList={this.props.surveyIdList}
+                        handleSelectedSurvey={this.props.handleSelectedSurvey}
+                        getSubdomain={this.props.getSubdomain}
+                    />
+                    {
+                        this.state.showCreateSurvey ?
+                            <CreateSurveyForm onClick={() => this.setState({ showCreateSurvey: false })}
+                                insertSurveyToDb={this.props.insertSurveyToDb}
+                                getSubdomain={this.props.getSubdomain} />
+                            :
+                            <Button onClick={() => this.setState({ showCreateSurvey: true })}>Add Survey</Button>
+                    }
+                </div>
+                <br></br>
+            </>
+        );
+    }
 }
 
 export default ActiveSurveyContainer;
